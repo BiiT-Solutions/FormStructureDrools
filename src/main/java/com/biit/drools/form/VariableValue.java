4723adcb7987f06576b6e933e648ed13e47762f2
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-02-16 13:20:13 +0100

    Added Variable Value Class

---
+++ src/main/java/com/biit/drools/form/VariableValue.java
@@ -0,0 +1,46 @@
+package com.biit.drools.form;
+
+/**
+ * Class for storing ABCD Custom Variables values.
+ */
+public class VariableValue {
+
+    //TreeObject name or comparationId;
+    private String reference;
+
+    //Variable Name
+    private String variable;
+
+    //Value of the variable
+    private Object value;
+
+    public VariableValue(String reference, String variable, Object value) {
+        this.reference = reference;
+        this.variable = variable;
+        this.value = value;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
